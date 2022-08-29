b7e722d62104e567e3ff74b831c8461489ca9858
jfitz <jacob.fitzgerald@icomera.com>
2022-08-29 23:34:34 +0100

    exporting months

--- txn_file_classes.py
+++ txn_file_classes.py
@@ -1,5 +1,6 @@
 import re
 import csv
+import datetime
 
 from catagories import catagorise
 
@@ -12,7 +13,7 @@ class TxnFile:
         :param filename str: csv filename found in /csv/<month>
         """
         self.filename: str = filename
-        self.txns: list = []
+        self.txns: dict = {}
 
 
     def extract_txns(self) -> None:
@@ -27,22 +28,25 @@ class Amex(TxnFile):
         """Takes a given amex csv filename and updates self.txns
         """
         temp_row = {}
-        self.txns = []
         with open(f'csv/{self.filename}', 'r') as csv_file:
             csv_reader = csv.reader(csv_file)
             for count, row in enumerate(csv_reader):
                 if count > 0: # Don't use header row
+                    month = datetime.datetime.strptime(row[0], "%d/%m/%Y")
                     temp_row['Date'] = row[0]
                     temp_row['Description'] = row[1]
-                    temp_row['Spender']('Jake')
+                    temp_row['Spender'] = 'Jake'
+                    temp_row['Catagory'] = catagorise(temp_row['Description'])
                     if 'jess' in self.filename:
                         temp_row['Price'] = float(row[4])
                         if 'MISS' in row[2]:
-                            temp_row['Spender']('Jess')
+                            temp_row['Spender'] = 'Jess'
                     else:
                         temp_row['Price'] = float(row[2])
-                    temp_row['Catagory'] = catagorise(temp_row['Description'])
-                    self.txns.append(temp_row)
+                    if month in self.txns:
+                        self.txns[month].append(temp_row)
+                    else:
+                        self.txns[month] = [temp_row]
 
 
 class Santander(TxnFile):
@@ -52,17 +56,24 @@ class Santander(TxnFile):
     def extract_txns(self) -> None:
         """Takes a given Santander csv filename and updates self.txns
         """
-        temp_row = {}
-        self.txns = []
-        with open(f'csv/{self.filename}', 'r') as csv_file:
+        with open(f'csv/{self.filename}', 'r', encoding='utf-8-sig') as csv_file:
             csv_reader = csv.reader(csv_file)
             for count, row in enumerate(csv_reader):
+                temp_row = {}
                 if count > 4: # Don't use header rows
+                    month = datetime.datetime.strptime(row[1], "%d/%m/%Y").month
                     temp_row['Date'] = row[1]
-                    temp_row['Description'] = re.sub(r'\sON\s(\d{2}-){2}\d{4}', '', row[3]).replace('CARD PAYMENT TO ', '')
-                    temp_row['Price'] = float(row[5].replace('£', '').replace(',', '')) * -1 if row[5] else float(row[6].replace('£', '').replace(',', '')) # Invert 'Money In' to be negative
                     temp_row['Catagory'] =  catagorise(row[3])
+                    temp_row['Description'] = re.sub(r'\sON\s(\d{2}-){2}\d{4}', '', row[3]).replace('CARD PAYMENT TO ', '').replace('FASTER PAYMENTS RECEIPT ', '')
+                    temp_row['Price'] = row[5] if row[5] else row[6]
+                    chars = ['£', ',', '\'']
+                    for c in chars:
+                        temp_row['Price'] = temp_row['Price'].replace(c, '')
+                    temp_row['Price'] = float(temp_row['Price']) * - 1 if row[5] else float(temp_row['Price'])
                     temp_row['Spender'] = 'Jake'
                     if 'jess' in self.filename:
                         temp_row['Spender'] = 'Jess'
-                    self.txns.append(temp_row)
+                    if month in self.txns:
+                        self.txns[month].append(temp_row)
+                    else:
+                        self.txns[month] = [temp_row]
--- update_finance.py
+++ update_finance.py
@@ -1,8 +1,11 @@
 import os
-
 import gspread
+import calendar
 
 from txn_file_classes import Amex, Santander
+
+
+_SPREADSHEET_NAME = 'Spend Tracker v2'
 
 def make_sheet(sheet_name) -> gspread.Worksheet:
     """Creates a new sheet from 'Template' sheet in worksheet
@@ -11,37 +14,56 @@ def make_sheet(sheet_name) -> gspread.Worksheet:
     :retrun: gspread Worksheet obj
     """
     gc = gspread.oauth()
-    client = gc.open("Spend Tracker v2")
+    client = gc.open(_SPREADSHEET_NAME)
     worksheet = client.worksheet("Template")
     worksheet = worksheet.duplicate()
     worksheet.update_title(sheet_name)
     return worksheet
 
 
-def export_to_sheet(txns: list, sheet: gspread.Worksheet) -> None:
+def export_to_sheet(txns: dict, sheet: gspread.Worksheet) -> None:
     """Takes txns list and exports to google sheets
     
-    :param txns list: list of transactions from csv files
+    :param txns dict: dict for a month of transactions from csv files
     :param sheet gspread.Worksheet: gspread Worksheet object to export to
     """
-    sheet.update(f'A2:E{len(txns) + 1}', txns)
+    sheet.update(f'A2:E{len(txns) + 1}', [[value for value in txn.values()] for txn in txns])
     sheet.sort((1, 'asc'), range=f'A2:E{len(txns) + 1}')
 
 
 if __name__ == "__main__":
-    month = input("Enter the month: ").lower()
+    # TODO
+    #   When exporting to sheet, export to corresponding month tab
+    #       Take month from filename and create dict = {<month>: <txns_list>}
+    #       Insert txns into corresponding tabs and sort
+    
+    #   Once a csv has had its txns exported, add to exported list and do not export again
+    #       Create txt of exported csv files
+    #       Append exported csv's to txt and check before opening
 
+    
     txn_files = []
-    for filename in os.listdir(f'csv/{month}'):
-        if month in filename:
-            if 'amex' in filename:
-                txn_files.append(Amex(filename))
-            if 'santander' in filename:
-                txn_files.append(Santander(filename))
-    txns = []
+    for filename in os.listdir(f'csv/'):
+        if 'amex' in filename:
+            txn_files.append(Amex(filename))
+        if 'santander' in filename:
+            txn_files.append(Santander(filename))
+
+    txns_to_export = {}
     for file in txn_files:
         file.extract_txns()
-        txns.extend(file.txns)
-    if txns:
-        worksheet = make_sheet(f'{month.upper()}_22')
-        export_to_sheet(txns, worksheet)
+        for month, txns in file.txns.items():
+            if month in txns_to_export:
+                txns_to_export[month].append(txns)
+            else:
+                txns_to_export[month] = txns
+
+    gc = gspread.oauth()
+    spreadsheet = gc.open(_SPREADSHEET_NAME)
+    for month, txns in txns_to_export.items():
+        title = f'{calendar.month_name[month]}_22' # TODO Make year dynamic
+        if title not in [sheet.title for sheet in spreadsheet.worksheets()]:
+            sheet = make_sheet(title) 
+        else:
+            sheet = spreadsheet.worksheet(title)
+        export_to_sheet(txns, sheet)
